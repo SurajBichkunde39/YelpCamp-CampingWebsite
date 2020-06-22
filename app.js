38c9290970c1ae22e4ad157e9194f901d006bb94
SurajBichkunde <ssbwork39@gamil.com>
2020-06-22 07:08:19 +0000

    adds custom stylesheet and styling to the show page.

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ seedDB();
 
 var app = express();
 app.set("view engine" , "ejs");
+app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
 // =========================================================
